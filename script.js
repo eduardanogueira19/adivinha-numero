1eca12a79f0a3e79945b79e48d307057a5868c49
Eduarda Nogueira <148588723+eduardanogueira19@users.noreply.github.com>
2026-04-02 20:47:36 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//Gerar um número aleatório
+let numeroSecreto = Math.floor(Math.random() * 11);
+/*
+    Math.random() → gera número entre 0 e 0.999...
+    * 101 → transforma em intervalo de 0 a 100
+    Math.floor() → arredonda pra baixo (vira inteiro)
+*/
+
+let acerto = false;
+let mensagem = "";
+let tentativas = 0;
+
+function btnChutar() {
+
+    let numeroUsuario = Number(document.getElementById("inputNumero").value);
+    let mensagem = "";
+
+    tentativas++;
+
+    if (numeroUsuario === numeroSecreto) {
+        mensagem = "Parabéns, você acertou!";
+        acerto = true;
+        
+    } 
+    else if (numeroUsuario > numeroSecreto) {
+        mensagem = "Seu chute foi MAIOR que o número secreto";
+    } 
+    else {
+        mensagem = "Seu chute foi MENOR que o número secreto";
+    }
+
+    document.getElementById("mensagem").textContent = mensagem;
+    document.getElementById("tentativas").textContent = tentativas;
+
+    inputNumero.value = "";
+}
+
+function btnReiniciar(){
+    numeroSecreto = Math.floor(Math.random() * 11);
+    tentativas = 0;
+    acerto = false;
+
+    document.getElementById("mensagem").textContent = "";
+    document.getElementById("tentativas").textContent = 0;
+    document.getElementById("inputNumero").value = "";
+}
+
+/*
+Quando o usuário acertar, não deixar continuar sem apertar o botão de reiniciar
+*/
